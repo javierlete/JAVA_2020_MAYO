80a6c6a9f16c5e616d8fd792ff1b60d0b2edae30
javaee <javaee@javaee-VirtualBox>
2020-05-29 14:22:03 +0200

    Comentarios

--- uf2404/src/uf2404/Aleatorio.java
+++ uf2404/src/uf2404/Aleatorio.java
@@ -5,12 +5,14 @@ import java.util.Date;
 public class Aleatorio {
 
 	public static void main(String[] args) {
+		// Ejemplo de fechas
 		Date d = new Date();
 		
 		System.out.println(d);
 		
 		System.out.println(d.getTime());
 		
+		// Ejemplo de aleatorios
 		System.out.println(new Date(30 * 24 * 60 * 60 * 1000));
 		
 		System.out.println(dado());
