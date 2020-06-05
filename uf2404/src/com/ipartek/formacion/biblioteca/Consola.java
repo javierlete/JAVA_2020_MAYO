25ba7f5740af8402bb0a752b5618d90ec315630a
javaee <javaee@javaee-VirtualBox>
2020-06-05 10:16:34 +0200

    Clase Consola para pedir datos por consola

---
+++ uf2404/src/com/ipartek/formacion/biblioteca/Consola.java
@@ -0,0 +1,35 @@
+package com.ipartek.formacion.biblioteca;
+
+import java.util.Scanner;
+
+public class Consola {
+	private static Scanner s = new Scanner(System.in);
+	
+	public static String leerString(String mensaje) {
+		mostrar(mensaje);
+		return s.nextLine();
+	}
+	
+	public static int leerInt(String mensaje) {
+		mostrar(mensaje);
+		int i = s.nextInt();
+		s.nextLine();
+		return i;
+	}
+	
+	public static double leerDouble(String mensaje) {
+		mostrar(mensaje);
+		double d = s.nextDouble();
+		s.nextLine();
+		return d;
+	}
+	
+	public static boolean leerBoolean(String mensaje) {
+		mostrar(mensaje + "(sí/no)");
+		return s.nextLine().matches("^[sS][iIíÍ]?$");
+	}
+	
+	private static void mostrar(String mensaje) {
+		System.out.print(mensaje + ": ");
+	}
+}
