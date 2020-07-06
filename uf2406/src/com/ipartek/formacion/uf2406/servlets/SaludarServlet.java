81c7b6614d59ff899b6c517298167f169f7b070e
Javier Lete <javier@eskura21.com>
2020-07-06 10:46:39 +0200

    SaludarServlet con formulario HTML embebido

--- uf2406/src/com/ipartek/formacion/uf2406/servlets/SaludarServlet.java
+++ uf2406/src/com/ipartek/formacion/uf2406/servlets/SaludarServlet.java
@@ -1,6 +1,8 @@
 package com.ipartek.formacion.uf2406.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +14,15 @@ public class SaludarServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/plain");
+		response.setContentType("text/html");
 		
 		String nombre = request.getParameter("nombre");
 		
-		response.getWriter().println("Hola " + nombre);
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html><html lang='es'><head><title>Saludo</title></head><body><h1>");
+		out.println("Hola " + nombre);
+		out.println("</h1><form action='saludar'><input name='nombre' placeholder='nombre' value='" + nombre + "' /><button>Aceptar</button></form>");
+		out.println("</body></html>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
