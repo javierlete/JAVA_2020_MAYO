1c66f59cb3fec40893ce88aca0bbfb7686ad89fb
Javier Lete <javier@eskura21.com>
2020-07-07 08:34:01 +0200

    Proyecto vacío calculadoraweb

---
+++ calculadoraweb/src/com/ipartek/formacion/ejercicios/calculadoraweb/servlets/CalculadoraServlet.java
@@ -0,0 +1,22 @@
+package com.ipartek.formacion.ejercicios.calculadoraweb.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/calculadora")
+public class CalculadoraServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
