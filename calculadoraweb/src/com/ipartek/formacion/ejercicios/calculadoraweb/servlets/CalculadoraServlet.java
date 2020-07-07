a88f5427ce29d7080097d495b9cd66f1629f0e47
Javier Lete <javier@eskura21.com>
2020-07-07 09:38:41 +0200

    Calculadoraweb: versión con formulario embebido

--- calculadoraweb/src/com/ipartek/formacion/ejercicios/calculadoraweb/servlets/CalculadoraServlet.java
+++ calculadoraweb/src/com/ipartek/formacion/ejercicios/calculadoraweb/servlets/CalculadoraServlet.java
@@ -13,37 +13,73 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/calculadora")
 public class CalculadoraServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Extraer toda la información recibida que necesitamos
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		// Extraer toda la información recibida que necesitamos
 		String op1 = request.getParameter("op1");
 		String op2 = request.getParameter("op2");
 		String op = request.getParameter("op");
-		
-		Double i1 = Double.parseDouble(op1);
-		Double i2 = Double.parseDouble(op2);
-		
+
+		PrintWriter out = response.getWriter();
+
 		Double res = null;
 		
-		switch(op) {
-		case "+": res = i1 + i2; break;
-		case "-": res = i1 - i2; break;
-		case "x": res = i1 * i2; break;
-		case "/": res = i1 / i2; break;
+		if (op1 != null && op2 != null && op != null) {
+			Double i1 = Double.parseDouble(op1);
+			Double i2 = Double.parseDouble(op2);
+
+			
+
+			switch (op) {
+			case "+":
+				res = i1 + i2;
+				break;
+			case "-":
+				res = i1 - i2;
+				break;
+			case "x":
+				res = i1 * i2;
+				break;
+			case "/":
+				res = i1 / i2;
+				break;
+			}
+
 		}
 		
-		DecimalFormat df = new DecimalFormat( "#0.00" );
-		
-		PrintWriter out = response.getWriter();
-		
-		if(res != null) {
-			out.println(op1 + op + op2 + "=" + df.format(res));
-		} else {
-			out.println("No se ha podido realizar la operación " + op);
-		}
+		DecimalFormat df = new DecimalFormat("#0.00");
+
+		out.println("<!DOCTYPE html>\n"
+				+ "<html>\n"
+				+ "<head>\n"
+				+ "<meta charset=\"UTF-8\">\n"
+				+ "<title>Calculadora</title>\n"
+				+ "<style>\n"
+				+ "	input {\n"
+				+ "		width: 4em;\n"
+				+ "	}\n"
+				+ "</style>\n"
+				+ "</head>\n"
+				+ "<body>\n"
+				+ "<form action=\"calculadora\">\n"
+				+ "		<input type=\"number\" step=\".01\" name=\"op1\" value=\"" + (op1 != null ? op1 : "") + "\" />\n"
+				+ "		<select name=\"op\">\n"
+				+ "			<option " + ("+".equals(op) ? "selected" : "") + ">+</option>\n"
+				+ "			<option " + ("-".equals(op) ? "selected" : "") + ">-</option>\n" 
+				+ "			<option " + ("x".equals(op) ? "selected" : "") + ">x</option>\n"
+				+ "			<option " + ("/".equals(op) ? "selected" : "") + ">/</option>\n"
+				+ "		</select>\n"
+				+ "		<input type=\"number\" name=\"op2\" step=\".01\" value=\"" + (op2 != null ? op2 : "") + "\"  />\n"
+				+ "		<button>=</button>\n" + (res != null ? df.format(res) : "") 
+				+ "	</form>\n"
+				+ "</body>\n"
+				+ "</html>");
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
