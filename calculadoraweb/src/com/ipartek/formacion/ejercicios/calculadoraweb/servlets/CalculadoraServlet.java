584665d28ed682b1a58a383f62e809dd7347b3f2
Javier Lete <javier@eskura21.com>
2020-07-07 09:03:11 +0200

    Versión básica de calculadora web

--- calculadoraweb/src/com/ipartek/formacion/ejercicios/calculadoraweb/servlets/CalculadoraServlet.java
+++ calculadoraweb/src/com/ipartek/formacion/ejercicios/calculadoraweb/servlets/CalculadoraServlet.java
@@ -1,6 +1,9 @@
 package com.ipartek.formacion.ejercicios.calculadoraweb.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +15,32 @@ public class CalculadoraServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//Extraer toda la información recibida que necesitamos
+		String op1 = request.getParameter("op1");
+		String op2 = request.getParameter("op2");
+		String op = request.getParameter("op");
+		
+		Double i1 = Double.parseDouble(op1);
+		Double i2 = Double.parseDouble(op2);
+		
+		Double res = null;
+		
+		switch(op) {
+		case "+": res = i1 + i2; break;
+		case "-": res = i1 - i2; break;
+		case "x": res = i1 * i2; break;
+		case "/": res = i1 / i2; break;
+		}
+		
+		DecimalFormat df = new DecimalFormat( "#0.00" );
+		
+		PrintWriter out = response.getWriter();
+		
+		if(res != null) {
+			out.println(op1 + op + op2 + "=" + df.format(res));
+		} else {
+			out.println("No se ha podido realizar la operación " + op);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
