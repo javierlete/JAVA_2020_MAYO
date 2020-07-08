72bb65ab32da3aa2b2efb592ffb876747c55d119
Javier Lete <javier@eskura21.com>
2020-07-08 11:42:09 +0200

    Cerrar sesión

---
+++ uf2406/src/com/ipartek/formacion/uf2406/servlets/SesionCerrar.java
@@ -0,0 +1,25 @@
+package com.ipartek.formacion.uf2406.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/sesioncerrar")
+public class SesionCerrar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().invalidate();
+		
+		response.sendRedirect("sesionproductos");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
