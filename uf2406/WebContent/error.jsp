c9016626289100452a8ade5b11aa45ab5095bb01
Javier Lete <javier@eskura21.com>
2020-07-10 10:08:51 +0200

    Versión inicial login

---
+++ uf2406/WebContent/error.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Error en la aplicación</title>
+</head>
+<body>
+	<h1>Ha habido un error no esperado en la aplicación</h1>
+</body>
+</html>
---
+++ uf2406/src/com/ipartek/formacion/uf2406/servlets/LoginServlet.java
@@ -0,0 +1,39 @@
+package com.ipartek.formacion.uf2406.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		if(email == null || password == null) {
+			//response.setStatus(Response.SC_BAD_REQUEST);
+			response.sendRedirect("error.jsp");
+			return;
+		}
+		
+		if(!"javierlete@email.net".equals(email) || !"contra".equals(password)) {
+			response.sendRedirect("errorlogin.jsp");
+			return;
+		}
+		
+		response.sendRedirect("principal.jsp");
+//		response.getWriter().println(email);
+//		response.getWriter().println(password);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
