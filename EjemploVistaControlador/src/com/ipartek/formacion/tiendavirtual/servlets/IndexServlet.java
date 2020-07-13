1641efaf9a1b76e8941754cb3bafe48af9d9ce80
Javier Lete <javier@eskura21.com>
2020-07-13 09:15:14 +0200

    Ruta /index para IndexController

--- EjemploVistaControlador/src/com/ipartek/formacion/tiendavirtual/servlets/IndexServlet.java
+++ EjemploVistaControlador/src/com/ipartek/formacion/tiendavirtual/servlets/IndexServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/IndexServlet")
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
