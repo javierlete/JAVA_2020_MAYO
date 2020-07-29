dc870c9a91cf7ee84c49df6324eb2816357debab
Javier Lete <javier@eskura21.com>
2020-07-29 12:19:10 +0200

    Cambios ligeros en el try

--- EjemploDAOsimple/src/com/ipartek/formacion/uf2175/ejemplodaosimple/controladores/ProductosListadoServlet.java
+++ EjemploDAOsimple/src/com/ipartek/formacion/uf2175/ejemplodaosimple/controladores/ProductosListadoServlet.java
@@ -17,26 +17,25 @@ import com.ipartek.formacion.uf2175.ejemplodaosimple.modelos.Producto;
 public class ProductosListadoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ArrayList<Producto> productos = null;
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
-			productos = ProductoDao.obtenerTodos();
+			ArrayList<Producto> productos = ProductoDao.obtenerTodos();
+
+			request.setAttribute("productos", productos);
+
+			request.getRequestDispatcher("productoslistado.jsp").forward(request, response);
 		} catch (SQLException e) {
 			response.getWriter().println("Error al obtener los productos");
 			e.printStackTrace(response.getWriter());
-			return;
 		} catch (ClassNotFoundException e) {
 			response.getWriter().println("No se ha encontrado la clase de driver de base de datos");
 			e.printStackTrace(response.getWriter());
 		}
-		
-		request.setAttribute("productos", productos);
-		
-		request.getRequestDispatcher("productoslistado.jsp").forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
