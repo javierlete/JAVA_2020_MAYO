2af06c68d47aa83c5bd946f1dce6c4836c51bc9d
Javier Lete <javier@eskura21.com>
2020-09-24 09:28:54 +0200

    Ejemplo de test JUnit4 con función sumar

---
+++ ejemploconsoladao/src/main/java/com/ipartek/formacion/ejemploconsoladao/Utilidades.java
@@ -0,0 +1,7 @@
+package com.ipartek.formacion.ejemploconsoladao;
+
+public class Utilidades {
+	public static int sumar(int a, int b) {
+		return a + b;
+	}
+}
--- ejemploconsoladao/src/test/java/com/ipartek/formacion/ejemploconsoladao/AppTest.java
+++ ejemploconsoladao/src/test/java/com/ipartek/formacion/ejemploconsoladao/AppTest.java
@@ -1,5 +1,6 @@
 package com.ipartek.formacion.ejemploconsoladao;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,7 +10,13 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
+    @Test
+    public void sumar() {
+    	int resultado = Utilidades.sumar(5, 3);
+    	assertEquals(8, resultado);
+    }
+	
+	/**
      * Rigorous Test :-)
      */
     @Test
