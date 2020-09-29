d42e3d9a1ab40890103970cb47a13b6e12663898
Javier Lete <javier@eskura21.com>
2020-09-29 08:44:01 +0200

    Proyecto inicial ejemploswingdao

---
+++ ejemploconsoladao/src/test/java/com/ipartek/formacion/ejemploconsoladao/ConsolaTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.ipartek.formacion.ejemploconsoladao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author javaee
+ *
+ */
+public class ConsolaTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.ipartek.formacion.ejemploconsoladao.Consola#pedirLinea(java.lang.String)}.
+	 */
+	@Test
+	public final void testPedirLinea() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.ipartek.formacion.ejemploconsoladao.Consola#pedirDouble(java.lang.String)}.
+	 */
+	@Test
+	public final void testPedirDouble() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.ipartek.formacion.ejemploconsoladao.Consola#pedirLong(java.lang.String)}.
+	 */
+	@Test
+	public final void testPedirLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
