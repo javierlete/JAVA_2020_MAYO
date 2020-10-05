9c3b08d87d566bab9a3f8fac71bb4d89da1a3e2b
Javier Lete <javier@eskura21.com>
2020-10-05 13:48:41 +0200

    Uso de ID como ruta y no como parámetro

--- ejemplorest/src/com/ipartek/formacion/ejemplorest/apirest/MuebleServlet.java
+++ ejemplorest/src/com/ipartek/formacion/ejemplorest/apirest/MuebleServlet.java
@@ -14,7 +14,7 @@ import com.google.gson.Gson;
 import com.ipartek.formacion.ejemplorest.accesodatos.MuebleDAO;
 import com.ipartek.formacion.ejemplorest.modelos.Mueble;
 
-@WebServlet("/api/muebles")
+@WebServlet("/api/muebles/*")
 public class MuebleServlet extends HttpServlet {
 	private static final Logger log = Logger.getLogger("REST_MUEBLES");
 
@@ -22,17 +22,20 @@ public class MuebleServlet extends HttpServlet {
 
 	private static final Gson gson = new Gson();
 
+	// GET http://localhost:8081/ejemplorest/api/muebles   --> TODOS los muebles
+	// GET http://localhost:8081/ejemplorest/api/muebles/3 --> Mueble cuyo id es el 3
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
 		
-		String id = request.getParameter("id");
+		String id = obtenerId(request);
 
 		log.info("id = " + id); //log.log(Level.INFO, "id = " + id);
 		
 		if (id == null) {
+			// TODOS
 			ArrayList<Mueble> muebles = MuebleDAO.obtenerTodos();
 			
 			log.info(muebles.toString());
@@ -41,6 +44,7 @@ public class MuebleServlet extends HttpServlet {
 			
 			response.getWriter().println(jsonMuebles);
 		} else {
+			// Mueble por id
 			Mueble mueble = MuebleDAO.obtenerPorId(Long.parseLong(id));
 			
 			log.info(mueble != null ? mueble.toString() : "No se ha encontrado el mueble");
@@ -50,6 +54,7 @@ public class MuebleServlet extends HttpServlet {
 		}
 	}
 
+	// POST http://localhost:8081/ejemplorest/api/muebles
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -59,6 +64,7 @@ public class MuebleServlet extends HttpServlet {
 		MuebleDAO.insertar(mueble);
 	}
 
+	// PUT http://localhost:8081/ejemplorest/api/muebles/3
 	@Override
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
@@ -67,10 +73,26 @@ public class MuebleServlet extends HttpServlet {
 		MuebleDAO.modificar(mueble);
 	}
 
+	// DELETE http://localhost:8081/ejemplorest/api/muebles/3
 	@Override
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("id");
+		String id = obtenerId(request);
 		
 		MuebleDAO.borrar(Long.parseLong(id));
+	}
+	
+	// Extraer del pathInfo el id, si no se encuentra null
+	private String obtenerId(HttpServletRequest request) {
+		String id = null;
+		
+		String pathInfo = request.getPathInfo();
+		
+		log.info(pathInfo);
+		
+		if(pathInfo != null && pathInfo.trim().length() != 0) {
+			id = pathInfo.substring(1);
+		}
+		
+		return id;
 	}
 }
