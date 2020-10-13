344e999b986a2a55beb3e11df84b1f8e28283d7f
Javier Lete <javier@eskura21.com>
2020-10-13 12:14:02 +0200

    Ejemplo Spring MVC starter

---
+++ ejemplospringmvc/src/main/java/com/ipartek/formacion/spring/ejemplospringmvc/EjemplospringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ejemplospringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplospringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplospringmvcApplication.class, args);
+	}
+
+}
---
+++ ejemplospringmvc/src/test/java/com/ipartek/formacion/spring/ejemplospringmvc/EjemplospringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ejemplospringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplospringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
