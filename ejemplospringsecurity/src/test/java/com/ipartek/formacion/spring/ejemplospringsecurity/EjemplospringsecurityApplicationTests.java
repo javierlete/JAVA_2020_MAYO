364d4f834c07d5f5a4e7b9b597f20af13b87c7ad
Javier Lete <javier@eskura21.com>
2020-10-26 08:45:53 +0100

    Proyecto inicial Login

---
+++ ejemplospringsecurity/src/test/java/com/ipartek/formacion/spring/ejemplospringsecurity/EjemplospringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ejemplospringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplospringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
