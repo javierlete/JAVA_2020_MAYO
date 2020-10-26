1a804a0341a4f178282728272c2a6a1d86663adb
Javier Lete <javier@eskura21.com>
2020-10-26 13:48:19 +0100

    Autenticacion con base de datos H2 (no funciona la consola h2)

--- ejemplospringsecurity/src/main/java/com/ipartek/formacion/spring/ejemplospringsecurity/WebSecurityConfig.java
+++ ejemplospringsecurity/src/main/java/com/ipartek/formacion/spring/ejemplospringsecurity/WebSecurityConfig.java
@@ -1,39 +1,49 @@
 package com.ipartek.formacion.spring.ejemplospringsecurity;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean // Necesario para que el objeto lo pueda crear el propio Spring Security para poder autenticar a los usuarios
-	@Override
-	protected UserDetailsService userDetailsService() {
-		@SuppressWarnings("deprecation")
-		UserDetails user =
-				 User.withDefaultPasswordEncoder()
-					.username("javier")
-					.password("contra")
-					.roles("USER")
-					.build();
-
-			return new InMemoryUserDetailsManager(user);
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication()
+	      .dataSource(dataSource)
+	      .withDefaultSchema()
+	      .withUser(User.withUsername("user")
+	        .password(passwordEncoder().encode("pass"))
+	        .roles("USER"));
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-			.antMatchers("/", "/home").permitAll()
-			.anyRequest().authenticated()
+			.antMatchers("/", "/home","/h2-console/**")
+			.permitAll()
+			.anyRequest()
+			.authenticated()
 			.and()
 		.formLogin()
 			.loginPage("/login")
@@ -41,6 +51,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 		.logout()
 			.permitAll();
+		
+		http.csrf().ignoringAntMatchers("/h2-consola/**");
+		http.headers()
+        .frameOptions()
+        .sameOrigin();
 	}
 
 }
